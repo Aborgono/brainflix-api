6961a5bd77c0f1fd6cd02187d04c717d24412080
Aborgono <albertoiborgonovo@gmail.com>
2023-11-07 11:44:56 -0500

    added try catch to users

--- api/videos.js
+++ api/videos.js
@@ -3,24 +3,61 @@ const router = express.Router()
 const fs = require('fs')
 const { v4: uuidv4 } = require('uuid');
 
-function readVideosFile() {
-    const videosList = fs.readFileSync("./data/videos.json");
-    const parsedData = JSON.parse(videosList);
-    return parsedData;
+async function readVideosFile() {
+    try {
+        const videosList = await fs.readFile("./data/videos.json", 'utf-8');
+        const parsedData = JSON.parse(videosList);
+        return parsedData;
+    } catch (error) {
+        // Handle the error and send an appropriate response
+        console.error("Error reading or parsing videos file:", error);
+        throw error; // You can choose to re-throw the error or send a response
+    }
 }
 
-
-router.get("/", (req, res) => {
-    const videos = readVideosFile();
-    res.json(videos);
+router.get("/", async (req, res) => {
+    try {
+        const videos = await readVideosFile();
+        res.json(videos);
+    } catch (error) {
+        res.status(500).json({ error: "Internal Server Error" });
+    }
 });
 
-router.get("/:videoID", (req, res) => {
-    const videos = readVideosFile();
-    const specificVideo = videos.find((video) => {
-        return video.id === req.params.videoID
-    })
-    res.json(specificVideo);
+router.get("/:videoID", async (req, res) => {
+    try {
+        const videos = await readVideosFile();
+        const specificVideo = videos.find((video) => {
+            return video.id === req.params.videoID;
+        });
+        if (specificVideo) {
+            res.json(specificVideo);
+        } else {
+            res.status(404).json({ error: "Video not found" });
+        }
+    } catch (error) {
+        res.status(500).json({ error: "Internal Server Error" });
+    }
 });
+
+// function readVideosFile() {
+//     const videosList = fs.readFileSync("./data/videos.json");
+//     const parsedData = JSON.parse(videosList);
+//     return parsedData;
+// }
+
+
+// router.get("/", (req, res) => {
+//     const videos = readVideosFile();
+//     res.json(videos);
+// });
+
+// router.get("/:videoID", (req, res) => {
+//     const videos = readVideosFile();
+//     const specificVideo = videos.find((video) => {
+//         return video.id === req.params.videoID
+//     })
+//     res.json(specificVideo);
+// });
 
 module.exports = router;
